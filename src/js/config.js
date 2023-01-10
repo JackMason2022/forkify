89c48364bcd6db7f3ee5039b017487bd25aa5cfb
JackMason2022 <thecurrentjc1@gmail.com>
2023-01-10 12:31:19 +1000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cf22a541-7dc0-48a3-806e-e9a355b80d82';
+export const MODAL_CLOSE_SEC = 2.5;
