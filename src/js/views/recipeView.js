c42c80a2163a149bc15eb066f82c810db9053eb2
JackMason2022 <thecurrentjc1@gmail.com>
2023-01-10 13:55:51 +1000

    Removed fraction string

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,7 +4,6 @@ import View from './View.js';
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 import fracty from 'fracty';
 
-import { Fraction } from 'fractional';
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = 'We could not find that recipe. Please try another one!';
@@ -129,7 +128,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? fracty(ing.quantity).toString() : ''
+        ing.quantity ? fracty(ing.quantity) : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
